93026d36caeed02533ddafbebacecea1dd321c2a
DaryllJim <117010240+DaryllJim@users.noreply.github.com>
2022-10-31 10:12:35 +0800

    update Hakdog.py

--- Hakdog.py
+++ Hakdog.py
@@ -1,33 +1,142 @@
-#list
-have_access = ["Omega","Daryll","Jack"]
-#variable
-name = "Daryll"
+#Japnese Quiz Game
 
-if name in have_access
-print("Access Granted")
+def score(point,score):
+  print(f"Score: {point)/{score}")
 
-name = "Jack"
-
-if name = = "Jack":
-    #code
-elif name = = "Omega":
-    #code
+def question():
+  point = 0
+  correct_points = 26
+  
+  print("What is dog in japanese")
+  anws = input()
+  if answ == "inu":
+    point += 1
     
-name = "Jack" 
-
-if name = = "Jack"
-    print("Access Granted")
-else:
-    print("Access Denied")
+  print("What is morning in japanese")
+  anws = input()
+  if answ == "asa":
+    point += 1
     
+  print("What is delicious in japanese")
+  anws = input()
+  if answ == "oishi":
+    point += 1
     
-
-
-
-
-
-
-
-
-
-
+  print("What is english in japanese")
+  anws = input()
+  if answ == "eigo":
+    point += 1
+    
+  print("What is happy in japanese")
+  anws = input()
+  if answ == "ureshi":
+    point += 1
+    
+  print("What is email in japanese")
+  anws = input()
+  if answ == "mēru":
+    point += 1
+    
+  print("What is four in japanese")
+  anws = input()
+  if answ == "yon":
+    point += 1
+    
+  print("What is text in japanese")
+  anws = input()
+  if answ == "mēru":
+    point += 1
+    
+  print("What is 'do' in japanese")
+  anws = input()
+  if answ == "dō":
+    point += 1
+    
+  print("What is phone in japanese")
+  anws = input()
+  if answ == "denwa":
+    point += 1
+    
+  print("What is dinner in japanese")
+  anws = input()
+  if answ == "ban gohan":
+    point += 1
+    
+  print("What is winter in japanese")
+  anws = input()
+  if answ == "fuyu":
+    point += 1
+    
+  print("What is leaf in japanese")
+  anws = input()
+  if answ == "ha":
+    point += 1
+    
+  print("What is hour in japanese")
+  anws = input()
+  if answ == "jikan":
+    point += 1
+    
+  print("What is stamps in japanese")
+  anws = input()
+  if answ == "kitte":
+    point += 1
+    
+  print("What is summer in japanese")
+  anws = input()
+  if answ == "natsu":
+    point += 1
+    
+  print("What is present in japanese")
+  anws = input()
+  if answ == "purezento":
+    point += 1
+    
+  print("What is profession in japanese")
+  anws = input()
+  if answ == "shoku-gyō":
+    point += 1
+    
+  print("What is weather in japanese")
+  anws = input()
+  if answ == "tenki":
+    point += 1
+    
+  print("What is wife in japanese")
+  anws = input()
+  if answ == "tsuma":
+    point += 1
+    
+  print("What is headache in japanese")
+  anws = input()
+  if answ == "zutsū":
+    point += 1
+    
+  print("What is 'we' in japanese")
+  anws = input()
+  if answ == "watashitachi":
+    point += 1
+    
+  print("What is beer in japanese")
+  anws = input()
+  if answ == "biru":
+    point += 1
+    
+  print("What is rice in japanese")
+  anws = input()
+  if answ == "gohan":
+    point += 1
+    
+  print("What is 9 in japanese")
+  anws = input()
+  if answ == "kyū":
+    point += 1
+    
+  print("What is apple in japanese")
+  anws = input()
+  if answ == "ringo":
+    point += 1
+    
+  score(point,correct_points)
+  
+question()
