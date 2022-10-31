78bb5efd8a91738b1c54d2db25c760582f3b2d5d
DaryllJim <117010240+DaryllJim@users.noreply.github.com>
2022-10-31 10:06:16 +0800

    update Hakdog.py

--- Hakdog.py
+++ Hakdog.py
@@ -1,1 +1,33 @@
-print("hello Jun Mark")
+#list
+have_access = ["Omega","Daryll","Jack"]
+#variable
+name = "Daryll"
+
+if name in have_access
+print("Access Granted")
+
+name = "Jack"
+
+if name = = "Jack":
+    #code
+elif name = = "Omega":
+    #code
+    
+name = "Jack" 
+
+if name = = "Jack"
+    print("Access Granted")
+else:
+    print("Access Denied")
+    
+    
+
+
+
+
+
+
+
+
+
+
