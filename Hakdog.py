988ed68696ab299cf6694bf38b48886698d2d468
DaryllJim <117010240+DaryllJim@users.noreply.github.com>
2022-10-31 13:27:27 +0800

    update Hakdog.py

--- Hakdog.py
+++ Hakdog.py
@@ -1,142 +1,35 @@
-#Japnese Quiz Game
+import pygame
+import sys
+pygame.init()
 
-def score(point,score):
-  print(f"Score: {point}/{score}")
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("pygame")
+width = 40
+height= 60
+x = 50
+y = 50
+velocity = 5
 
-def question():
-  point = 0
-  correct_points = 26
+
+run = True
+while run:
+  pygame.time.delay(100)
   
-  print("What is dog in japanese")
-  answ = input()
-  if answ == "inu":
-    point += 1
-    
-  print("What is morning in japanese")
-  answ = input()
-  if answ == "asa":
-    point += 1
-    
-  print("What is delicious in japanese")
-  answ = input()
-  if answ == "oishi":
-    point += 1
-    
-  print("What is english in japanese")
-  answ = input()
-  if answ == "eigo":
-    point += 1
-    
-  print("What is happy in japanese")
-  answ = input()
-  if answ == "ureshi":
-    point += 1
-    
-  print("What is email in japanese")
-  answ = input()
-  if answ == "mēru":
-    point += 1
-    
-  print("What is four in japanese")
-  answ = input()
-  if answ == "yon":
-    point += 1
-    
-  print("What is text in japanese")
-  answ = input()
-  if answ == "mēru":
-    point += 1
-    
-  print("What is 'do' in japanese")
-  answ = input()
-  if answ == "dō":
-    point += 1
-    
-  print("What is phone in japanese")
-  answ = input()
-  if answ == "denwa":
-    point += 1
-    
-  print("What is dinner in japanese")
-  answ = input()
-  if answ == "ban gohan":
-    point += 1
-    
-  print("What is winter in japanese")
-  answ = input()
-  if answ == "fuyu":
-    point += 1
-    
-  print("What is leaf in japanese")
-  answ = input()
-  if answ == "ha":
-    point += 1
-    
-  print("What is hour in japanese")
-  answ = input()
-  if answ == "jikan":
-    point += 1
-    
-  print("What is stamps in japanese")
-  answ = input()
-  if answ == "kitte":
-    point += 1
-    
-  print("What is summer in japanese")
-  answ = input()
-  if answ == "natsu":
-    point += 1
-    
-  print("What is present in japanese")
-  answ = input()
-  if answ == "purezento":
-    point += 1
-    
-  print("What is profession in japanese")
-  answ = input()
-  if answ == "shoku-gyō":
-    point += 1
-    
-  print("What is weather in japanese")
-  answ = input()
-  if answ == "tenki":
-    point += 1
-    
-  print("What is wife in japanese")
-  answ = input()
-  if answ == "tsuma":
-    point += 1
-    
-  print("What is headache in japanese")
-  answ = input()
-  if answ == "zutsū":
-    point += 1
-    
-  print("What is 'we' in japanese")
-  answ = input()
-  if answ == "watashitachi":
-    point += 1
-    
-  print("What is beer in japanese")
-  answ = input()
-  if answ == "biru":
-    point += 1
-    
-  print("What is rice in japanese")
-  answ = input()
-  if answ == "gohan":
-    point += 1
-    
-  print("What is 9 in japanese")
-  answ = input()
-  if answ == "kyū":
-    point += 1
-    
-  print("What is apple in japanese")
-  answ = input()
-  if answ == "ringo":
-    point += 1
-    
-  score(point,correct_points)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+      
+  keys = pygame.key.get_pressed()
   
-question()
+  if keys[pygame.K_LEFT]:
+    x -= velocity
+  if keys[pygame.K_RIGHT]:
+    x += velocity
+  if keys[pygame.K_UP]:
+    y -= velocity
+  if keys[pygame.K_DOWN]:
+    y += velocity
+  
+  win.fill((0,0,0))
+  pygame.draw.rect(win,(255,0,0), (x,y,width,height))
+  pygame.display.update()
